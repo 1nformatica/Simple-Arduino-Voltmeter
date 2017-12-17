d473aceab8485f7c3749ad7d557c805c490bcc08
1nformatica <gregcann@gmail.com>
2017-12-17 19:10:28 +0100

    Add files via upload
    Testing an OLED module sourced from Banggood. The OLED has 128 * 32 pixel screen and I2C serial Interface. It runs from 5 volts. I show a simple test configuration using an Arduino Uno to drive the display and use an analogue input as a voltmeter.
    OLED Display Module 12832 LCD Screen IIC I2C http://bit.ly/OLED128_32 Arduino Starter Kit http://bit.ly/ArduinoStarter Silicone Blue Desk Mat http://bit.ly/BlueDeskMat OLED USB2.0 Voltage Current Power Tester http://bit.ly/USB_Testr Geiger Counter https://www.youtube.com/watch?v=4Tqo3...
    
    
    Click Here to Subscribe http://bit.ly/1nformatica
    Good source of electronic parts: http://bit.ly/ElecComps
    Follow on Facebook https://www.facebook.com/1nformatica

---
+++ Simple_Arduino_Voltmeter.ino
@@ -0,0 +1,52 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h> // Graphics Library
+#include <Adafruit_SSD1306.h> //OLED Library
+           
+
+int analogInput = 0;
+float vout = 0.0;
+float vin = 0.0;
+float R1 = 1000000.0; // resistance of R1 (1M)
+float R2 = 100000.0; // resistance of R2 (100K) 
+int value = 0;
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup(){
+   Serial.begin(9600);
+   pinMode(analogInput, INPUT);
+   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+   display.clearDisplay();  // Clear the buffer.
+   display.setTextSize(2);   // text display Voltmeter
+   display.setTextColor(WHITE);
+   display.setCursor(0,0);
+   display.print("Volt Meter");
+   display.display();
+   display.setTextSize(2);
+   display.setTextColor(WHITE);
+   display.print("Vin = ");
+   display.display();
+   Serial.print("got this far!"); 
+
+}
+void loop(){
+   value = analogRead(analogInput); // read analog input
+   vout = (value * 5.0) / 1024.0; // 
+   vin = vout / (R2/(R1+R2)); 
+   if (vin<0.09) {
+   vin=0.0;// make zero
+   
+} 
+   display.setTextSize(2);   // text display Voltmeter
+   display.setTextColor(WHITE,BLACK);
+   display.setCursor(70,18);
+   display.print(vin);
+   display.setTextColor(WHITE);
+   display.setCursor(70,18);
+   display.print(vin);
+   display.display();
+   delay(500);
+}
+   
